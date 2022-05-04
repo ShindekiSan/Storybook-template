dfcd9194b0fca6e41ff9003145f2ec216bcef2c6
ShindekiSan <abelchuk.maksim@gmail.com>
2022-05-04 16:08:21 +0300

    added storybook addon for docs, improved story for button, mdx story for button

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,17 @@
 module.exports = {
   "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials"],
+  "addons": [
+    "@storybook/addon-links",
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+        babelOptions: {},
+        sourceLoaderOptions: null,
+      },
+    },
+    "@storybook/addon-essentials"
+  ],
   "framework": "@storybook/web-components",
   core: {
     builder: "webpack5"
--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -9,25 +9,53 @@ interface IButton {
 }
 
 export default {
-  title: 'Button',
+  title: 'Components/Button',
   component: 'custom-button',
   parameters: {
+    docs: {
+      inlineStories: false,
+    },
     actions: {
       handles: ['click'],
     },
+    componentSubtitle: 'Button is the primary component for user to interact with UI.',
   },
   argTypes: {
     variant: {
+      description: 'Button styles preset',
       options: ['contained', 'outlined', 'text'],
-      control: { type: 'radio' },
+      control: { type: 'select', defaultValue: 'contained' },
+      table: {
+        type: { summary: 'string' }
+      },
+      defaultValue: { summary: 'contained' },
+    },
+    text: {
+      description: 'Button text',
+      table: {
+        type: { summary: 'string' }
+      },
+      defaultValue: { summary: '' },
     },
   },
 } as Meta
 
 const Template: Story<IButton> = (args: IButton) => html`<custom-button variant=${args.variant} text=${args.text}></custom-button>`;
 
-export const Default = Template.bind({});
-Default.args = {
+export const Contained = Template.bind({});
+Contained.args = {
   text: 'Click me!',
   variant: 'contained',
+}
+
+export const Outlined = Template.bind({});
+Outlined.args = {
+  text: 'Click me!',
+  variant: 'outlined',
+}
+
+export const Text = Template.bind({});
+Text.args = {
+  text: 'Click me!',
+  variant: 'text',
 }
